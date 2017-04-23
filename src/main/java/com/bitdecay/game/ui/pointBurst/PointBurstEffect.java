dd1711bec0453a3e64cf319be7cf54858e9c1a06
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2017-04-23 10:07:50 -0600

    Moved point burst creation to factory.

--- src/main/java/com/bitdecay/game/ui/pointBurst/PointBurstEffect.java
+++ src/main/java/com/bitdecay/game/ui/pointBurst/PointBurstEffect.java
@@ -1,4 +1,4 @@
-package com.bitdecay.game.ui;
+package com.bitdecay.game.ui.pointBurst;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -12,21 +12,6 @@ import com.bitdecay.game.MyGame;
 
 public class PointBurstEffect extends Group {
     final float DURATION = 2;
-
-    public static PointBurstEffect money(float diff) {
-        Image moneyIcon = new Image(MyGame.ATLAS.findRegion("uiStuff/btc-512"));
-        moneyIcon.setScale(0.075f);
-
-        Image diffIcon;
-        if (diff > 0) {
-            diffIcon = new Image(MyGame.ATLAS.findRegion("uiStuff/plus"));
-        } else {
-            diffIcon = new Image(MyGame.ATLAS.findRegion("uiStuff/minus"));
-        }
-        diffIcon.setScale(0.5f);
-
-        return new PointBurstEffect(moneyIcon, diffIcon, Float.toString(diff));
-    }
 
     public PointBurstEffect(Image mainIcon, Image secondaryIcon, String text) {
         addActor(mainIcon);

d00aa22aecf81bd469f7b3ed55baf30e42d4cc00
TannerMoore <shadoninja@gmail.com>
2017-04-23 20:17:11 -0600

    under buildings!

--- src/main/java/com/bitdecay/game/room/DemoRoom.java
+++ src/main/java/com/bitdecay/game/room/DemoRoom.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -42,6 +43,8 @@ public class DemoRoom extends AbstractRoom {
     private Stage stage;
     TiledMap map;
     OrthogonalTiledMapRenderer renderer;
+    TiledMap roofMap;
+    OrthogonalTiledMapRenderer roofRenderer;
     NodeGraph graph;
 
     float scaleFactor = 1/40f;
@@ -138,6 +141,9 @@ public class DemoRoom extends AbstractRoom {
         map = new TmxMapLoader().load(Gdx.files.internal("img/tiled/town.tmx").path());
         renderer = new OrthogonalTiledMapRenderer(map, scaleFactor);
 
+        roofMap = new TmxMapLoader().load(Gdx.files.internal("img/tiled/town_roof.tmx").path());
+        roofRenderer = new OrthogonalTiledMapRenderer(roofMap, scaleFactor);
+
         MapLayers mapLayers = map.getLayers();
 
         TiledMapTileLayer collidablesLayer = (TiledMapTileLayer) mapLayers.get("Collidables");
@@ -231,6 +237,8 @@ public class DemoRoom extends AbstractRoom {
         renderer.setView(camera);
         renderer.render();
         super.draw(spriteBatch);
+        roofRenderer.setView(camera);
+        roofRenderer.render();
         stage.act(1/60f);
         stage.draw();
     }

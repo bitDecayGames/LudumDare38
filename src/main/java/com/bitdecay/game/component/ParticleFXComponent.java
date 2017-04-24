691a6535c2b1aef6c5a120a7eafbd236eacb3b97
Logan Moore <monday.hopscotch@gmail.com>
2017-04-24 09:39:32 -0600

    fix particles for exhaust

--- src/main/java/com/bitdecay/game/component/ParticleFXComponent.java
+++ src/main/java/com/bitdecay/game/component/ParticleFXComponent.java
@@ -2,12 +2,19 @@ package com.bitdecay.game.component;
 
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Monday on 4/22/2017.
  */
 public class ParticleFXComponent extends DrawableComponent {
+
+    public static ParticleEmitter.ScaledNumericValue zeroEmission = new ParticleEmitter.ScaledNumericValue();
+
     public ParticleEffect effect;
     public boolean continuous;
 
@@ -17,11 +24,22 @@ public class ParticleFXComponent extends DrawableComponent {
     public boolean requestStart = true;
     public boolean requestStop;
 
+    public Map<ParticleEmitter, ParticleEmitter.ScaledNumericValue> saved = new HashMap<>();
+
     public ParticleFXComponent(boolean continuous, FileHandle effectFile, FileHandle particleDir) {
         this.continuous = continuous;
         effect = new ParticleEffect();
         effect.load(effectFile, particleDir);
         effect.scaleEffect(.01f);
+
+        for (ParticleEmitter particleEmitter : effect.getEmitters()) {
+            if (!saved.containsKey(particleEmitter)) {
+                saved.put(particleEmitter, new ParticleEmitter.ScaledNumericValue());
+            }
+            ParticleEmitter.ScaledNumericValue scaledNumericValue = saved.get(particleEmitter);
+            scaledNumericValue.setLow(particleEmitter.getEmission().getLowMin(), particleEmitter.getEmission().getLowMax());
+            scaledNumericValue.setHigh(particleEmitter.getEmission().getHighMin(), particleEmitter.getEmission().getHighMax());
+        }
     }
 
     @Override

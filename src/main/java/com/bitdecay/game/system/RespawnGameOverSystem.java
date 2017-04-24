206504355c4b6dcd4a336aaf69d83997c3999f6e
Logan Moore <monday.hopscotch@gmail.com>
2017-04-24 11:38:47 -0600

    handle respawn in all death cases

--- src/main/java/com/bitdecay/game/system/RespawnGameOverSystem.java
+++ src/main/java/com/bitdecay/game/system/RespawnGameOverSystem.java
@@ -48,7 +48,7 @@ public class RespawnGameOverSystem extends AbstractUpdatableSystem {
         // Add camera and other stats
         car.addComponent(new CameraFollowComponent());
         car.addComponent(new PlayerControlComponent());
-        car.addComponent(new HungerComponent(100, 0.5f));
+        car.addComponent(new HungerComponent(3, 0.5f));
         car.addComponent(new PoopooComponent(100, 0.5f));
         car.addComponent(new MoneyComponent(100));
         car.addComponent(new PlayerBodyComponent());
@@ -56,10 +56,9 @@ public class RespawnGameOverSystem extends AbstractUpdatableSystem {
         PhysicsComponent p = car.getComponent(PhysicsComponent.class).get();
         for (JointEdge jointEdge : p.body.getJointList()) {
             MyGameObject tire = (MyGameObject) jointEdge.other.getUserData();
+            tire.addComponent(new PlayerTireComponent());
             if (tire.hasComponent(RevoluteJointComponent.class)) {
-                tire.addComponent(new PlayerControlComponent());
                 tire.addComponent(new DriveTireComponent(maxSpeed, acceleration));
-                tire.addComponent(new PlayerTireComponent());
                 tire.addComponent(new SteerableComponent(MathUtils.PI / 6));
             }
         }
